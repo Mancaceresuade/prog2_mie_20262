1b4ec150da81f9ab56bfbe65090d0a9fbbd25ffe
Mancaceresuade <mancaceres@uade.edu.ar>
2026-08-12 19:13:49 -0300

    inicio

---
+++ clase2/untitled/src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numeros = {5,6,7,8};
+        numeros[3] = 10;
+        // imprimirLista(numeros);
+        int[][] matriz = {{4,5,9},{1,2,3},{2,3,1},{5,6,4}};
+        imprimirPorFila(matriz);
+        imprimirPorColumna(matriz);
+    }
+
+    private static void imprimirPorColumna(int[][] matriz) {
+        for (int c = 0; c < matriz[0].length; c++) {
+            imprimirColumna(matriz,c);
+            System.out.println("");
+        }
+    }
+    private static void imprimirColumna(int[][] matriz, int c) {
+        for (int f = 0; f < matriz.length; f++) {
+            System.out.print(matriz[f][c] + " ");
+        }
+    }
+
+    private static void imprimirPorFila(int[][] matriz) {
+        for (int f = 0; f < matriz.length; f++) {
+            imprimirLista(matriz[f]);
+            System.out.println(" ");
+            /*
+            for (int c = 0; c < matriz[f].length; c++) {
+                System.out.println(matriz[f][c]);
+            }
+             */
+        }
+    }
+
+    private static void imprimirLista(int[] numeros) {
+        for (int i=0; i < numeros.length; i++) {
+            System.out.print(numeros[i]+ " ");
+        }
+    }
+
+
+}
